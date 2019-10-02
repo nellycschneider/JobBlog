aeb067d4b8b439b0f255deb0b13f4a1fc2c842d6
Nelly Schneider <nellschge@stadtlengsfeld.net>
2019-10-02 11:26:17 +0200

    authentication

---
+++ server 2/client/src/App.js
@@ -0,0 +1,62 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import { Route } from "react-router-dom";
+// import Projects from "./components/Projects";
+// import ProjectDetails from "./components/ProjectDetails";
+// import TaskDetails from "./components/TaskDetails";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+
+class App extends React.Component {
+  state = {
+    user: this.props.user
+  };
+
+  setUser = user => {
+    this.setState({
+      user: user
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar user={this.state.user} setUser={this.setUser} />
+        {/* <Route exact path="/projects" component={Projects} /> */}
+
+        {/* if there's no logged in user, don't show the projects page */}
+        {/* <Route
+          exact
+          path="/projects"
+          render={props => {
+            if (this.state.user) return <Projects {...props} />;
+            else return <Redirect to={"/"} />;
+          }}
+        />
+
+        <Route
+          exact
+          path="/projects/:id"
+          render={props => <ProjectDetails {...props} user={this.state.user} />}
+        /> */}
+
+        {/* <Route exact path="/tasks/:id" component={TaskDetails} /> */}
+        <Route
+          exact
+          path="/signup"
+          render={props => <Signup setUser={this.setUser} {...props} />}
+        />
+        <Route
+          exact
+          path="/login"
+          render={props => <Login setUser={this.setUser} {...props} />}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ server 2/routes/auth.js
@@ -0,0 +1,93 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+
+//signup route with POST
+router.post("/signup", (req, res) => {
+  const { username, password } = req.body;
+
+  if (password.length < 8) {
+    //status 400 to tell the server that something went wrong
+    return res
+      .status(400)
+      .json({ message: "Your password must be 8 characters minimum." });
+  }
+
+  if (!username) {
+    return res.status(400).json({ message: "Your username cannot be empty." });
+  }
+  //if there is already a user with that username
+  User.findOne({ username: username })
+    .then(found => {
+      if (found) {
+        return res
+          .status(400)
+          .json({ message: "This username is already taken." });
+      }
+
+      //if not found
+      const salt = bcrypt.genSaltSync();
+      const hash = bcrypt.hashSync(password, salt);
+
+      //if you use return here, the catch of the other then will catch the errors
+      //User.create() returns a promise (if it doesn't fulfill in the .then(), it will be caught in the .catch() afterwards)
+      return User.create({ username: username, password: hash }).then(
+        dbUser => {
+          req.login(user, err => {
+            if (err) {
+              return res
+                .status(500)
+                .json({ message: "Error while attempting to login." });
+            }
+            res.json(dbUser);
+          });
+        }
+      );
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+//login route with POST
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  //use passport for authentication
+  passport.authenticate("local", (err, user) => {
+    //if non-user related error, error during de-serializing phase of passport
+    if (err) {
+      return res.status(500).json({ message: "Error during authentication." });
+    }
+    //no user with provided credentials there
+    if (!user) {
+      return res.status(400).json({ message: "Wrong credentials." });
+    }
+
+    req.login(user, err => {
+      if (err) {
+        return res
+          .status(500)
+          .json({ message: "Error while attempting to login." });
+      }
+      return res.json(user);
+    });
+  })(req, res);
+});
+
+//logout route
+//DELETE /api/auth/logout
+router.delete("/logout", (req, res) => {
+  req.logout();
+  res.json({ message: "Successful logout." });
+});
+
+//check if the user has an active session (so that he will stay logged in)
+//GET /api/auth/loggedin
+router.get("/loggedin", (req, res) => {
+  res.json(req.user);
+});
+
+module.exports = router;
