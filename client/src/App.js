5b34889bdc61cf8a1b9805d8be7d2cb7be0b92d9
Moritz Wright <me@moritzwright.com>
2019-10-07 16:50:46 +0200

    merging

--- client/src/App.js
+++ client/src/App.js
@@ -40,7 +40,6 @@ class App extends React.Component {
         {/* <Route exact path="/projects" component={Projects} /> */}
 
         {/* if there's no logged in user, don't show the projects page */}
-<<<<<<< HEAD
         <div className="container">
           <Switch>
             <Route exact path="/" user={this.state.user} component={Home} />
@@ -60,61 +59,6 @@ class App extends React.Component {
             <Route exact path="/cv/all" render={props => <AllCVs setUser={this.setUser} {...props} />} />
 
             <Route exact path="/cv/:id" render={props => <FinishedCV setUser={this.setUser} {...props} />} />
-=======
-        <Switch>
-          <Route exact path="/" user={this.state.user} component={Home} />
-          {/* ================BLOG Router===================== */}
-          <Route
-            exact
-            path="/portfolio"
-            user={this.state.user}
-            component={Portfolio}
-          />
-          <Route
-            exact
-            path="/portfolio/dashboard"
-            component={PortfolioDashboard}
-          />
-          <Route
-            path="/portfolio/dashboard/create-new"
-            component={CreateNewProject}
-          />
-          <Route
-            path="/portfolio/dashboard/edit-project/:id"
-            component={EditProject}
-          />
-          <Route path="/portfolio/project/:id" component={Project} />
-          {/* ================BLOG Router===================== */}
-
-          {/* <Route exact path="/tasks/:id" component={TaskDetails} /> */}
-          <Route
-            exact
-            path="/signup"
-            render={props => <Signup setUser={this.setUser} {...props} />}
-          />
-          <Route
-            exact
-            path="/login"
-            render={props => <Login setUser={this.setUser} {...props} />}
-          />
-          <Route
-            exact
-            path="/cv/form"
-            render={props => <UserForm setUser={this.setUser} {...props} />}
-          />
-
-          <Route
-            exact
-            path="/cv/all"
-            render={props => <AllCVs setUser={this.setUser} {...props} />}
-          />
-
-          <Route
-            exact
-            path="/cv/:id"
-            render={props => <FinishedCV setUser={this.setUser} {...props} />}
-          />
->>>>>>> eaa705194cb0856d19e084effd1b942b913f63da
 
             {/* <Route
             exact
