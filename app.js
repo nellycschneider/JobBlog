410d3ea6da4bfade7a70a323a163e8ce13fb5af4
Moritz Wright <me@moritzwright.com>
2019-10-09 15:43:01 +0200

    env

--- app.js
+++ app.js
@@ -53,7 +53,9 @@ app.use(
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "/client/build")));
+
 app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 
 // ADD SESSION SETTINGS HERE:
@@ -96,5 +98,10 @@ app.use("/api/auth", authRoutes);
 
 const userRoutes = require("./routes/userform");
 app.use("/cv", userRoutes);
+
+app.use((req, res) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/client/build/index.html");
+});
 
 module.exports = app;
