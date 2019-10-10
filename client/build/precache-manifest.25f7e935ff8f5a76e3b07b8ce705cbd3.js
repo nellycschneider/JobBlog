09b465e4ace62cecb24de05f61a0079ff63eee22
Nelly Schneider <nellschge@stadtlengsfeld.net>
2019-10-10 18:46:23 +0200

    deploy

---
+++ client/build/precache-manifest.25f7e935ff8f5a76e3b07b8ce705cbd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b180400c29d2ec41c59e14253c827b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77285fc610b96d0b14cd",
+    "url": "/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "b70a2bca420180ae902d",
+    "url": "/static/css/main.b45e7fad.chunk.css"
+  },
+  {
+    "revision": "77285fc610b96d0b14cd",
+    "url": "/static/js/2.c936ebb7.chunk.js"
+  },
+  {
+    "revision": "b70a2bca420180ae902d",
+    "url": "/static/js/main.1600143a.chunk.js"
+  },
+  {
+    "revision": "7f45875b99a5ef02142c",
+    "url": "/static/js/runtime-main.c2763ce9.js"
+  }
+]);
