87245578c54bfea0af24c52de62ed3398dc1d6ec
Nelly Schneider <nellschge@stadtlengsfeld.net>
2019-10-10 18:02:39 +0200

    merge

--- app.js
+++ app.js
@@ -19,14 +19,18 @@ mongoose
     useNewUrlParser: true
   })
   .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
   })
   .catch(err => {
     console.error("Error connecting to mongo", err);
   });
 
 const app_name = require("./package.json").name;
-const debug = require("debug")(`${app_name}:${path.basename(__filename).split(".")[0]}`);
+const debug = require("debug")(
+  `${app_name}:${path.basename(__filename).split(".")[0]}`
+);
 
 const app = express();
 
