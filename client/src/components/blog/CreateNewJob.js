fcce3f445bbec80246d4a6a0d46203bb9f8f25c5
Moritz Wright <me@moritzwright.com>
2019-10-10 18:41:58 +0200

    final

--- client/src/components/blog/CreateNewJob.js
+++ client/src/components/blog/CreateNewJob.js
@@ -47,57 +47,34 @@ export default class CreateNewJob extends Component {
   render() {
     return (
       <div>
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={this.handleSubmit} className="form-create jobForm">
           <div className="title">
             <label htmlFor="title">Job Title: </label>
-            <input
-              type="text"
-              name="title"
-              id="title"
-              onChange={this.handleChange}
-              value={this.state.title}
-            />
+            <input type="text" name="title" id="title" onChange={this.handleChange} value={this.state.title} />
           </div>
 
           <div className="description">
             <label htmlFor="jobDescription">Job Description: </label>
-            <textarea
-              name="jobDescription"
-              id="jobDescription"
-              cols="30"
-              rows="10"
-              onChange={this.handleChange}
-              value={this.state.jobDescription}
-            ></textarea>
+            <textarea name="jobDescription" id="jobDescription" cols="30" rows="10" onChange={this.handleChange} value={this.state.jobDescription}></textarea>
           </div>
 
           <div className="link">
             <label htmlFor="link">Link: </label>
-            <input
-              type="text"
-              name="link"
-              id="link"
-              onChange={this.handleChange}
-              value={this.state.link}
-            />
+            <input type="text" name="link" id="link" onChange={this.handleChange} value={this.state.link} />
           </div>
 
-          <select
-            value={this.state.type}
-            name="type"
-            onChange={this.handleChange}
-          >
+          <select value={this.state.type} name="type" onChange={this.handleChange} className="btn">
             <option value="Choose a type">Choose a type</option>
             <option value="UI/UX">UI/UX</option>
             <option value="Frontend">Frontend</option>
-            <option value="Backend">
-              Backend
-            </option>
+            <option value="Backend">Backend</option>
             <option value="Fullstack">Fullstack</option>
             <option value="Data">Data</option>
           </select>
 
-          <button type="submit">Create Post</button>
+          <button type="submit" className="btn">
+            Create Job
+          </button>
         </form>
       </div>
     );
